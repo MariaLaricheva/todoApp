e8a71d0e7092fc01bc99f24593c20b490dba920b
Мария Ларичева <43138491+MariaLaricheva@users.noreply.github.com>
2022-08-31 17:03:52 +0300

    v 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Task from "./components/Task"
+import TaskInput from "./components/TaskInput"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            tasks: [
+                {id: 0, title: "Create to-do app with React", done: false},
+                {id: 1, title: "Add MobX to the project", done: true},
+                {id: 2, title: "Add MobX to the metaclass project", done: false}
+            ]
+        }
+    }
+
+    doneTask = (id) => {
+        const index = this.state.tasks.map(task => task.id).indexOf(id);
+        this.setState(state => {
+            let tasks = state.tasks;
+            tasks[index].done = true;
+            return tasks;
+        })
+    }
+
+    deleteTask = (id) => {
+        const index = this.state.tasks.map(task => task.id).indexOf(id);
+        this.setState(state => {
+            let tasks = state.tasks;
+            delete tasks[index];
+            // почему-то эта строчка не работает:
+            // tasks.filter(task => task.id !== index)
+            return tasks;
+        })
+    }
+
+    addTask = task => {
+        this.setState(state => {
+            let { tasks } = state;
+            let newTask = {
+                id: tasks.length!== 0 ? tasks.length : 0,
+                title: task,
+                done: false
+            };
+            tasks.push(newTask);
+            return tasks;
+        })
+    }
+
+    render() {
+
+        const { tasks } = this.state;
+        const activeTasks = tasks.filter(task => !task.done);
+        const doneTasks = tasks.filter(task => task.done);
+
+
+        return(
+            <div className="App">
+
+                <h1 className="top">
+                    Active tasks: {activeTasks.length}
+                </h1>
+
+                {[...activeTasks, ...doneTasks].map(task => (
+                    <Task
+                        task={task}
+                        key={task.id}
+                        //если передать функцию - она выполнится при рендере компонента
+                        //а стрелочная функция сразу не выполнится
+                        // поэтому используем стрелочные!!!
+                        doneTask={() => this.doneTask(task.id)}
+                        deleteTask={() => this.deleteTask(task.id)}
+                    />
+                ))}
+
+                <TaskInput
+                addTask={this.addTask}
+                />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/TaskInput.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export class TaskInput extends React.Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            input: '',
+        };
+    }
+
+    addTask = () => {
+        const {input} = this.state;
+        if(input){
+            this.props.addTask(input);
+            this.setState({input:''})
+        }
+    }
+
+    inputChange = event => {
+        this.setState({input: event.target.value})
+    }
+
+    handleEnter = event => {
+        console.log("нажата клавиша!")
+        if (event.key === "Enter"){
+            this.addTask();
+        }
+    }
+
+    render() {
+        const {input} = this.state;
+
+        return(
+            <div className="Task input">
+                <input
+                    onChange={this.inputChange} value={input}
+                    onKeyPress={this.handleEnter}
+                />
+                <button
+                    onClick={this.addTask}
+                >ADD</button>
+            </div>
+        )
+    }
+}
+
+export default TaskInput;
